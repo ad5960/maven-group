d344ecff75f4a78d234199fef752ab3fb57d047c
Sutiksh Verma <verma.su@northeastern.edu>
2024-05-01 19:26:21 -0700

    Initial Commit

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+import MavenLogo from "../assets/MavenLogo.png"
+import Image from 'next/image'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className='fixed top-0 left-0 flex flex-row w-full py-10 bg-transparent z-50'>
+      <div className='flex flex-3 mx-20 py-0'>
+        <Image src={MavenLogo} alt="Maven Group logo" width={140} height={100}/>
+      </div>
+      <div className='flex flex-1 flex-row justify-end mx-20 list-none text-lg'>
+        <Link href="/about" className='px-5'>About</Link>  
+        <Link href="/properties" className='px-5'>Properties</Link>
+        <Link href="/careers" className='px-5'>Careers</Link>
+        <Link href="/contact_us" className='px-5'>Contact Us</Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
