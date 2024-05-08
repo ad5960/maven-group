2beef54eab9d910e7ed22824901a1b10f62c8406
Sutiksh Verma <verma.su@northeastern.edu>
2024-05-08 14:04:53 -0700

    Updated Properties Page

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,19 +1,42 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
-import MavenLogo from "../assets/MavenLogo.png"
+import React, { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import BlackMavenLogo from "../assets/MavenLogo.png"
+import WhiteMavenLogo from "../assets/Maven_white.png"
 import Image from 'next/image'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const pathname = usePathname()
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isPropertiesPage = pathname === '/properties';
+  
   return (
-    <nav className='fixed top-0 left-0 flex flex-row w-full py-10 bg-transparent z-50 overflow-hidden'>
-      <div className='flex flex-3 mx-20 py-0'>
-        <Image src={MavenLogo} alt="Maven Group logo" width={140} height={100} />
+    <nav className={`fixed top-0 left-0 flex flex-row w-full py-10 ${scrolled || isPropertiesPage? 'bg-white text-black' : 'bg-transparent text-white'} z-50 overflow-hidden`}>
+      <div className='flex flex-3 mx-20 my-[-30px]'>
+        <Image src={scrolled || isPropertiesPage ? BlackMavenLogo : WhiteMavenLogo} alt="Maven Group logo" width={140} height={100} />
       </div>
-      <div className='flex flex-1 flex-row justify-end mx-20 list-none text-white text-lg'>
+      <div className='flex flex-1 flex-row justify-end mx-20 list-none text-lg'>
+        <Link href="/" className='px-5'>Home</Link>
         <Link href="/about" className='px-5'>About</Link>
         <Link href="/properties" className='px-5'>Properties</Link>
+        <Link href="/services" className='px-5'>Services</Link>
         <Link href="/careers" className='px-5'>Careers</Link>
         <Link href="/contact_us" className='px-5'>Contact Us</Link>
       </div>
---
+++ src/app/properties/page.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "../components/nav";
+import SantaMonica from "../assets/SantaMonica.jpg"
+import PropertyCard from "../components/property_card";
+export default function Page() {
+    return (
+        <>
+            <Navbar/>
+
+            <div className="w-full h-10 mt-32">
+                <span className="w-1/2 justify-center items-center"><p className="text-4xl font-bold text-center">Properties</p></span>
+            </div>
+            <div className="flex w-full h-20 justify-center items-center px-24 my-10">
+                <div className="flex flex-row w-2/3 h-full bg-slate-300  justify-around items-center rounded-lg">
+                    <select className="w-1/3 h-1/2 mx-2 border border-slate-400 rounded-lg">
+                        <option value="option1">Location</option>
+                        <option value="option2">Culver City</option>
+                        <option value="option3">Los Angeles</option>
+                    </select>
+                    <select className="w-1/3 h-1/2 mx-2 border border-slate-400 rounded-lg">
+                        <option value="option1">Type</option>
+                        <option value="option2">Commercial</option>
+                        <option value="option3">Industrial</option>
+                    </select>
+                    <button className="w-1/3 h-1/2 mx-2 bg-slate-700 text-white rounded-lg">
+                        Update
+                    </button>
+                </div>
+            </div>
+            <div className="flex flex-col my-20 justify-center items-center">
+                <div className="flex flex-row m-5">
+                    <PropertyCard/>
+                    <PropertyCard />
+                    <PropertyCard />
+                </div>
+                <div className="flex flex-row m-5">
+                    <PropertyCard />
+                    <PropertyCard />
+                    <PropertyCard />
+                </div>
+                <div className="flex flex-row m-5">
+                    <PropertyCard />
+                    <PropertyCard />
+                    <PropertyCard />
+                </div>
+            </div>
+            {/* <div className="w-full h-full flex flex-col">
+                <div className="mt-28 ml-24 flex justify-center items-start">
+                    <div className="w-2/3">
+                        <p className="text-4xl font-bold ">Properties</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col my-20 justify-center items-center">
+                    <div className="flex flex-row m-5">
+                        <PropertyCard />
+                        <PropertyCard />
+                        <PropertyCard />
+                    </div>
+                    <div className="flex flex-row m-5">
+                        <PropertyCard />
+                        <PropertyCard />
+                        <PropertyCard />
+                    </div>
+                    <div className="flex flex-row m-5">
+                        <PropertyCard />
+                        <PropertyCard />
+                        <PropertyCard />
+                    </div>
+                </div>
+            </div> */}
+        </>
+    )
+}
